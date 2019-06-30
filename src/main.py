8851d6ae6c92757bc2f6e9c56fdc191511086b8a
nip403 <41174986+nip403@users.noreply.github.com>
2019-06-30 13:36:18 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,15 +1,22 @@
-import pygame
-from MainClasses import Player,Game
+#objective:
+#get to the end whilst not dying
+#time limit  
+#avoid enemies and collect powerups
+#prison theme? - rename
+
+import pygame 
+from engine import Player,Game
 
 s = [1000,800]
+
 pygame.init()
 screen = pygame.display.set_mode(s,0,32,0)
 clock = pygame.time.Clock()
-pygame.display.set_caption("Platformer Alpha 0.0")
+pygame.display.set_caption("Adventurer Beta")
 
 _details = type("Details",(),{"surf":screen,"clock":clock,"size":s})
 
-def main():
+def main(): 
     player = Player(250,250) 
     game = Game(player,_details)
     game.run()

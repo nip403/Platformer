4e09e135886c0277e28c332216c961b2b5312bf2
nip403 <41174986+nip403@users.noreply.github.com>
2019-07-01 05:39:53 +0100

    Removed the need for a clock

--- src/main.py
+++ src/main.py
@@ -5,20 +5,18 @@
 #prison theme? - rename
 
 import pygame 
-from engine import Player,Game
+from engine import Game
 
 s = [1000,800]
 
 pygame.init()
 screen = pygame.display.set_mode(s,0,32,0)
-clock = pygame.time.Clock()
 pygame.display.set_caption("Adventurer Beta")
 
-_details = type("Details",(),{"surf":screen,"clock":clock,"size":s})
+_details = type("Details",(),{"surf":screen,"size":s})
 
 def main(): 
-    player = Player(250,250) 
-    game = Game(player,_details)
+    game = Game(_details)
     game.run()
     
 if __name__ == "__main__":
